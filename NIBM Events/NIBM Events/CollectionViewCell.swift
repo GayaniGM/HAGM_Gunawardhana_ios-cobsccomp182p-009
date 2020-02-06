804f8a6e372f08c08ff13d7c76da3ef2baebe965
Gayani Gunawardhana <59130188+GayaniGM@users.noreply.github.com>
2020-02-07 02:13:36 +0530

    Added Event Home View Page

---
+++ NIBM Events/NIBM Events/CollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CollectionViewCell.swift
+//  NIBM Events
+//
+//  Created by Imali Chathurika on 2/7/20.
+//  Copyright © 2020 Gayani Madhushika. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var eventImage: UIImageView!
+    @IBOutlet weak var eventName: UILabel!
+    @IBOutlet weak var eventVenue: UILabel!
+    @IBOutlet weak var eventDateTime: UILabel!
+    
+    
+    
+}
